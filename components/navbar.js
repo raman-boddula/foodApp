f5e3ce0f27ffe95ac3ddd2de271042b8f92a3200
raman-boddula <raman_rc001@yahoo.com>
2021-10-26 14:57:27 +0530

    added new features

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 function navbar (){
     return `<div id="navbar">
-    <h3><a href="search.html"><img src="https://www.foodapp.us/images/logo.png" alt=""></a></h3>
+    <h3><a href="index.html"><img src="https://www.foodapp.us/images/logo.png" alt=""></a></h3>
     <input type="text" placeholder="Search your favourite dish" id="search">
     <button id="btn">Search</button>
     <div id="options">
--- scripts/showData.js
+++ scripts/showData.js
@@ -10,11 +10,12 @@ async function getDetails(url){
 function AppendData(data,container){
     data.forEach(({strMeal,strMealThumb,strIngredient1,strMeasure1,strIngredient2,strMeasure2,strIngredient3,strMeasure3,strIngredient4,strMeasure4,strIngredient5,strMeasure5})=>{
         let div = document.createElement('div');
-
+        div.setAttribute('class','pic')
         let p = document.createElement('h3');
         p.setAttribute('class','mealName')
         p.textContent= strMeal;
         let img = document.createElement('img');
+        
         img.src=strMealThumb;
         let inc = document.createElement('h4');
         inc.textContent="Incredients:- "+strIngredient1+"-"+strMeasure1+", "+strIngredient2+"-"+strMeasure2+", "+strIngredient3+"-"+strMeasure3+", "+strIngredient4+"-"+strMeasure4+", "+strIngredient5+"-"+strMeasure5;
@@ -23,5 +24,19 @@ function AppendData(data,container){
         container.append(div);
     })
 }
+function show(data,container){
+    data.forEach((res)=>{
+        let div = document.createElement('div');
+        div.setAttribute('class','pic')
+        let p = document.createElement('h2');
+       p.setAttribute('class','catName')
+        p.textContent= res.strCategory;
+        let img = document.createElement('img');
+        
+        img.src=res.strCategoryThumb;
+        div.append(img,p);
+        container.append(div);
+    })
+}
 
-export {getDetails,AppendData};
+export {getDetails,AppendData,show};
